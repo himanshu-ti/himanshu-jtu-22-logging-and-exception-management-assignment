e340e726438d0dbadc04538b65ac77aa69131f5a
codermaddy <hiindia.himanshu@gmail.com>
2022-06-14 19:06:20 +0530

    Adds logging, execution time info, exception handling to services/verify_phone_and_email.py

--- fast_api_als/services/verify_phone_and_email.py
+++ fast_api_als/services/verify_phone_and_email.py
@@ -13,14 +13,21 @@ How can you write log to understand what's happening in the code?
 You also trying to undderstand the execution time factor.
 """
 
+logging.basicConfig(format = '%(levelname)s %(asctime)s %(message)s')
+
 async def call_validation_service(url: str, topic: str, value: str, data: dict) -> None:  # 2
+    start  = time.process_time()
     if value == '':
         return
     async with httpx.AsyncClient() as client:  # 3
+        logging.info("Awaiting client response")
         response = await client.get(url)
+        logging.info("Got client response")
 
     r = response.json()
     data[topic] = r
+    time_taken = (time.process_time() - start) * 1000
+    logging.info("call_validation_service: Took %f ms", time_taken)
     
 
 async def verify_phone_and_email(email: str, phone_number: str) -> bool:
@@ -38,14 +45,27 @@ async def verify_phone_and_email(email: str, phone_number: str) -> bool:
     phone_valid = False
     data = {}
 
+    logging.info("Starting Email and Phone validation service")
     await asyncio.gather(
         call_validation_service(email_validation_url, "email", email, data),
         call_validation_service(phone_validation_url, "phone", phone_number, data),
     )
-    if "email" in data:
-        if data["email"]["DtResponse"]["Result"][0]["StatusCode"] in ("0", "1"):
-            email_valid = True
-    if "phone" in data:
-        if data["phone"]["DtResponse"]["Result"][0]["IsValid"] == "True":
-            phone_valid = True
-    return email_valid | phone_valid
+    logging.info("Received response from Email and Phone validation service")
+
+    try:    
+        if "email" in data:
+            if data["email"]["DtResponse"]["Result"][0]["StatusCode"] in ("0", "1"):
+                logging.info("Valid Email %s", data)
+                email_valid = True
+        if "phone" in data:
+            if data["phone"]["DtResponse"]["Result"][0]["IsValid"] == "True":
+                logging.info("Valid Phone Number")
+                phone_valid = True
+    except KeyError as key_error:
+        logging.error("Data doesn't contain key %s", key_error)
+        raise Exception("Data in incorrect form")
+    except Exception as e:
+        logging.error("Error occurred %s", e)
+        raise Exception(e)
+        
+    return email_valid | phone_valid    
